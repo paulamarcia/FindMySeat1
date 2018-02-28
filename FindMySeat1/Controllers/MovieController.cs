32f6f1a96ceeb62eb7658577820abdc073517e0c
paulamarcia30 <paulamarcia30@gmail.com>
2018-02-27 19:15:54 -0600

    first commit

---
+++ FindMySeat1/Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FindMySeat1.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+
+            List<string> movies = new List<string>();
+
+            movies.Add("Black Panther");
+            movies.Add("Annihilation");
+            movies.Add("Game Night");
+            movies.Add("Fifty Shades Freed");
+            movies.Add("Peter Rabbit");
+            movies.Add("Jumanji: Welcome to the Jungle");
+
+
+            ViewBag.movies = movies;
+
+
+
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
